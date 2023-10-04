3ea6dc3a8725e71f2a030b810ad2f460142ea5dc
henry <hmarichal93@gmail.com>
2023-10-04 20:18:08 -0300

    job  script

---
+++ cluster.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH --job-name=yolov8
+#SBATCH --ntasks=1
+#SBATCH --mem=2048
+#SBATCH --time=12:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --tmp=100G
+#SBATCH --mail-user=henry.marichal@fing.edu.uy
+
+# de acuerdo a lo que quiera ejecutar puede elegir entre las siguientes tres líneas.
+#SBATCH --gres=gpu:1 # se solicita una gpu cualquiera( va a tomar la primera que quede disponible indistintamente si es una p100 o una a100)
+
+
+#SBATCH --partition=normal
+#SBATCH --qos=gpu
+
+source /etc/profile.d/modules.sh
+source /clusteruy/home/henry.marichal/miniconda3/etc/profile.d/conda.sh
+conda activate yolov8
+
+# -------------------------------------------------------
+#disco local SSD local al nodo. /clusteruy/home/henry.marichal se accede via NFS (puede ser realmente lento)
+#el espacio local a utilizar se reserva con --tmp=XXXGb
+LOCAL_NODE_DIR=/scratch/henry.marichal
+
+#other variables
+RESULTADOS_DIR=$LOCAL_NODE_DIR/yolov8
+DATASET_DIR=$LOCAL_NODE_DIR/dataset_pith/yolo_urudendro
+HOME_RESULTADOS_DIR=~/resultados/yolov8
+HOME_DATASET_DIR=~/dataset_pith/yolo_urudendro
+# Define a function to check the result of a command
+check_command_result() {
+    # Run the command passed as an argument
+    "$@"
+
+    # Check the exit status
+    if [ $? -eq 0 ]; then
+        echo "Command was successful."
+    else
+        echo "Command failed with an error."
+        exit 1
+    fi
+}
+
+#copy dataset
+check_command_result mkdir -p $DATASET_DIR
+
+check_command_result cp  -r $HOME_DATASET_DIR $DATASET_DIR
+
+#training model
+cd ~/repos/yolov8/
+#./run_yolo.sh $RESULTADOS_DIR $DATASET_DIR
+touch $RESULTADOS_DIR/resultado.txt
+
+
+# -------------------------------------------------------
+#copy results
+mkdir -p $HOME_RESULTADOS_DIR
+cp -r $RESULTADOS_DIR/* $HOME_RESULTADOS_DIR
